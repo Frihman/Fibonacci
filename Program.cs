962f47fc847644bb2b94b840d1ab4560e2f48ba5
James Frih <jamesfrih@me.com>
2021-01-12 14:16:33 +0100

    first iteration

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fibonacci
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = new int[21];
+            
+            numbers[0] = 0;
+            numbers[1] = 1;
+            Console.WriteLine(numbers[0]);
+            Console.WriteLine(numbers[1]);
+
+            for (int i = 2; i < numbers.Length; i++)
+            {
+
+                numbers[i] = numbers[i - 2] + numbers[i - 1];
+                Console.WriteLine(numbers[i]);
+            }
+        }
+    }
+}
